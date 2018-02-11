7ec569e3b26e8fab30cfe4e98b419135685a6d37
Alex Utiansky <a.utiansky@gmail.com>
2018-02-12 00:18:25 +0300

    vk

---
+++ Cocktails.Identity.Services/VkService .cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Cocktails.Common.Exceptions;
+using Cocktails.Identity.ViewModels;
+using RestSharp;
+
+namespace Cocktails.Identity.Services
+{
+    public class VkService : ISocialService<VkUser>
+    {
+        private const string FacebookBaseUrl = "https://api.vk.com/method";
+        private const string FacebookDataUrl = "account.getProfileInfo&access_token={0}";
+
+        public async Task<VkUser> GetProfileAsync(string accessToken, CancellationToken cancellationToken)
+        {
+            var requestUrl = string.Format(FacebookDataUrl, accessToken);
+
+            var client = new RestClient(FacebookBaseUrl);
+            var request = new RestRequest(requestUrl, Method.GET);
+
+            var response = await client.ExecuteTaskAsync<VkUser>(request, cancellationToken);
+            if (!response.IsSuccessful)
+            {
+                throw new BadRequestException("Invalid oauth token");
+            }
+            var user = response.Data;
+
+            //user.PictureUrl = user.Picture.Data.Url;
+
+            return user;
+        }
+    }
+}
---
+++ Cocktails.Identity.ViewModels/VkUser.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Cocktails.Identity.ViewModels
+{
+    public class VkUser : SocialUserBase
+    {
+        [JsonProperty("first_name")]
+        public string FirstName { get; set; }
+
+        [JsonProperty("last_name")]
+        public string LastName { get; set; }
+
+        [JsonProperty("sex")]
+        public int Sex { get; set; }
+
+        [JsonProperty("bdate")]
+        [DataType(DataType.Date)]
+        public DateTimeOffset? Birthday { get; set; }
+    }
+}
